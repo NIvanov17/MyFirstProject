3c8af6a2def280b7f02b6542852118d7520a0af0
NIvnov17 <nikolai.ivanov171717@gamil.com>
2023-03-17 22:45:46 +0200

    Spring security setup

--- src/main/java/com/example/myfirstproject/config/SecurityConfiguration.java
+++ src/main/java/com/example/myfirstproject/config/SecurityConfiguration.java
@@ -1,8 +1,16 @@
 package com.example.myfirstproject.config;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfiguration {
 
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.build();
+    }
 }
